97e90463e319d2f6cf16a0313f591754149b7b8c
Kopano Shabalala <ST10443100@rcconnect.edu.za>
2025-06-27 20:58:38 +0000

    GUI with chatbot interface (WPF)

---
+++ CyberChatBotWPF/CyberTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CyberChatBotWPF
+{
+    public class CyberTask
+    {
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public DateTime? ReminderTime { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title}" + (ReminderTime.HasValue ? $" (Remind at {ReminderTime:g})" : "");
+        }
+    }
+}
